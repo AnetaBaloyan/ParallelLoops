9666fbf09cd4beefa3052437c6aa2e3c5c04e4c0
Aneta Baloyan <anetabaloyan@Anets-MacBook-Pro.local>
2018-04-17 21:49:26 +0400

    first commit

---
+++ Parallel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NewParallel
+{
+    public class MyParallel
+    {
+        public static void ParallelFor(int fromInclusive, int toExclusive, Action<int> body)
+        {
+            for (int i = fromInclusive; i < toExclusive; i++)
+            {
+                int container = i;
+                Task.Run(() => { body(container); });
+            }
+        }
+
+        public static void ParallelForEach<TSource>(IEnumerable<TSource> source, Action<TSource> body)
+        {
+            foreach (TSource item in source)
+            {
+                Task.Run(() => { body(item); });
+            }
+        }
+
+        public static void ParallelForEachWithOptions<TSource>
+        (IEnumerable<TSource> source, ParallelOptions options, Action<TSource> body)
+        {
+            using (SemaphoreSlim concurrencySemaphore = new SemaphoreSlim(options.MaxDegreeOfParallelism))
+            {
+                List<Task> tasks = new List<Task>();
+
+                foreach (TSource item in source)
+                {
+                    concurrencySemaphore.Wait();
+
+                    var t = Task.Factory.StartNew(() =>
+                    {
+
+                        try
+                        {
+                            body(item);
+                        }
+                        finally
+                        {
+                            concurrencySemaphore.Release();
+                        }
+                    });
+
+                    tasks.Add(t);
+                }
+
+                Task.WaitAll(tasks.ToArray());
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace NewParallel
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //MyParallel.ParallelFor(0, 5, (i) => print(i));
+
+            List<int> list = new List<int>{11, 2, -1, 4, 52, 6, 7, 0, 9};
+
+            //MyParallel.ParallelForEach(list, (i) => { i++; Console.WriteLine(i);} );
+
+            ParallelOptions p = new ParallelOptions();
+            p.MaxDegreeOfParallelism = 4;
+
+            MyParallel.ParallelForEachWithOptions(list, p, (i) => print(i));
+
+
+
+            void print(int i)
+            {
+                Console.WriteLine("Task {0} has started.", i);
+                Thread.Sleep(2000);
+                Console.WriteLine("Task {0} has ended.", i);
+
+            }
+
+
+            Console.Read();
+        }
+    }
+}
